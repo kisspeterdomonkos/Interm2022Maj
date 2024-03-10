50b4a242bcdb75675bbabb0db4280fa377bbda74
Kiss Péter Domonkos <72004868+kisspeterdomonkos@users.noreply.github.com>
2024-03-10 10:11:25 +0100

    Add files via upload

---
+++ k info ism 2022 maj/megoldások/SuperBowlGUI/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SuperBowlGUI
+{
+    partial class SuperBowlGUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.switchButton = new System.Windows.Forms.Button();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.romanTextBox = new System.Windows.Forms.TextBox();
+            this.arabicTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // switchButton
+            // 
+            this.switchButton.Location = new System.Drawing.Point(234, 43);
+            this.switchButton.Name = "switchButton";
+            this.switchButton.Size = new System.Drawing.Size(65, 31);
+            this.switchButton.TabIndex = 0;
+            this.switchButton.Text = "--->";
+            this.switchButton.UseVisualStyleBackColor = true;
+            this.switchButton.Click += new System.EventHandler(this.switchButton_Click);
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(234, 129);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(65, 31);
+            this.changeButton.TabIndex = 1;
+            this.changeButton.Text = "Átvált";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Római szám [I-X]:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Arab szám [1-10]:";
+            // 
+            // romanTextBox
+            // 
+            this.romanTextBox.Location = new System.Drawing.Point(49, 84);
+            this.romanTextBox.Name = "romanTextBox";
+            this.romanTextBox.Size = new System.Drawing.Size(85, 27);
+            this.romanTextBox.TabIndex = 4;
+            // 
+            // arabicTextBox
+            // 
+            this.arabicTextBox.Enabled = false;
+            this.arabicTextBox.Location = new System.Drawing.Point(379, 84);
+            this.arabicTextBox.Name = "arabicTextBox";
+            this.arabicTextBox.Size = new System.Drawing.Size(85, 27);
+            this.arabicTextBox.TabIndex = 4;
+            // 
+            // SuperBowlGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 222);
+            this.Controls.Add(this.arabicTextBox);
+            this.Controls.Add(this.romanTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.switchButton);
+            this.Name = "SuperBowlGUI";
+            this.Text = "Átváltó";
+            this.Load += new System.EventHandler(this.SuperBowlGUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button switchButton;
+        private Button changeButton;
+        private Label label1;
+        private Label label2;
+        private TextBox romanTextBox;
+        private TextBox arabicTextBox;
+    }
+}
---
+++ k info ism 2022 maj/megoldások/megoldasok.sql
@@ -0,0 +1,19 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 9. feladat:
+CREATE DATABASE vedettmadarak DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+
+-- 11. feladat:
+UPDATE faj SET ertek = 25 WHERE id = 352;
+
+-- 12. feladat:
+SELECT nev, latin FROM faj WHERE evszam IS NULL ORDER BY nev ASC;
+
+-- 13. feladat:
+SELECT evszam, COUNT(id) AS 'fajok száma' FROM faj WHERE evszam >= 2000 GROUP BY evszam;
+
+-- 14. feladat:
+SELECT faj.nev AS 'faj', csalad.nev AS 'család', rend.nev AS 'rend', faj.ertek*1000 AS 'eszmei érték' FROM faj INNER JOIN csalad ON faj.csaladId = csalad.id INNER JOIN rend ON rend.id = csalad.rendId WHERE faj.ertek>500;
+
+-- 15. feladat:
+SELECT csalad.nev, AVG(faj.ertek) AS 'átlag' FROM faj INNER JOIN csalad ON faj.csaladId = csalad.id GROUP BY csalad.nev;
